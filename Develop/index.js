f0528916290f4f6705322b0af44c0e2300d60ac6
jennj <jennjenn93@gmail.com>
2022-04-02 13:14:32 -0500

    adding description and title questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your README? (Required)',
+        
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a detailed description for your README. (Required)',
+        },
+    ]
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
