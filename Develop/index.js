90d5a2143aad11ff74cf2cc7044438cc3acfab9f
jennj <jennjenn93@gmail.com>
2022-04-03 10:35:21 -0500

    creating a string in console to test

--- Develop/index.js
+++ Develop/index.js
@@ -1,27 +1,30 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+//const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your README? (Required)',
+//const questions = () => {
+  //  return inquirer.prompt([
+    //    {
+      //      type: 'input',
+        //    name: 'title',
+          //  message: 'What is the title of your README? (Required)',
         
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a detailed description for your README. (Required)',
-        },
-    ]
-}
+        //},
+        //{
+         //   type: 'input',
+           // name: 'description',
+           // message: 'Provide a detailed description for your README. (Required)',
+        //},
+    //]};
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile('./develop/README.md', data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
+
+const generatePage = () => 'Name: Runbuddy, Github: runbuddy@github';
