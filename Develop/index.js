15d1a3186b05c4c0659533afcb958ed876d1469d
jennj <jennjenn93@gmail.com>
2022-04-03 19:11:06 -0500

    adding readmen markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,24 +1,23 @@
 const inquirer = require('inquirer');
 
-//const fs = require('fs');
+const fs = require('fs');
 
-//const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 //const readme = generateMarkdown(name, github);
-    
-  //  fs.writeFile('./createdREADME.md', readme, err => {
-    //    if (err) throw err;
 
-      //  console.log('README complete! Look at the createdREADME.md!');
+//  fs.writeFile('./createdREADME.md', readme, err => {
+//    if (err) throw err;
 
-   // });
+//  console.log('README complete! Look at the createdREADME.md!');
+
+// });
 
 // TODO: Include packages needed for this application
 //const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
+const questions = [
         {
             type: 'input',
             name: 'title',
@@ -27,8 +26,8 @@ const questions = () => {
                 if (titleInput) {
                     return true;
                 } else {
-                console.log('Please enter your title!');
-                return false;
+                    console.log('Please enter your title!');
+                    return false;
                 }
             }
         },
@@ -36,56 +35,133 @@ const questions = () => {
             type: 'input',
             name: 'description',
             message: 'Provide a detailed description for your project for the README. (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'installation',
             message: 'Provide instructions for the user to install and use your project. (Required)',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your installation instructions!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'usage',
             message: 'How will this project be used? (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your usage!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
             name: 'license',
             message: 'Choose a license that will best fit your project. (Required)',
             choices: ['MIT', 'GNU', 'Apache', 'BSD', 'Mozilla', 'None of the above'],
+            validate: licenseInput => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    console.log('Please select a license!');
+                    return false;
+                }
+            }
+
         },
         {
             type: 'input',
             name: 'contribute',
             message: 'What are some guidelines to contribute to this project? (Required)',
+            validate: contributeInput => {
+                if (contributeInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your contribute guidelines!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'test',
             message: 'How will the user test this project? (Required)',
+            validate: testInput => {
+                if (testInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your test!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
             message: 'What is your gitHub username? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your github!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'email',
             message: 'Please enter your email address for any questions about this project. (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email!');
+                    return false;
+                }
+            }
         }
-    ]);
-};
-
-    questions()
-    .then(answers => console.log(answers));
+    ];
 
 // TODO: Create a function to write README file
-//function writeToFile('./develop/README.md', data) {}
+function writeToFile('./develop/createdREADME.md', data) {
+fs.writeFile('./createdREADME.md', data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+    console.log("You can preview your README file now!");
+
+});
+};
 
 // TODO: Create a function to initialize app
-//function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile('./createdREADME.md', generatedMarkdown(userInput));
+    });
+};
+
 
 // Function call to initialize app
-//init();
+init();
 
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,46 @@
+
+const generateMarkdown = () => {
+  return `
+  
+  # ${data.title}
+
+  ## License
+  [![license](https://img.shields.io/badge/license-${data.license}-green.svg)] (https://shields.io/)
+
+  ## Tabls of Contents
+  - [Description] (#description)
+  - [Istallation] (#installation)
+  - [Usage] (#usage)
+  - [Contribution] (#contribution)
+  - [Test] (#test)
+  - [Questions?] (#questions?)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## Questions?
+  - GitHub: [${data.github}](https://github.com/${data.github})
+
+  - Email: [${data.email}](mailto:user@example.com)
+  `;
+  };
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 //function renderLicenseBadge(license) {}
@@ -12,14 +55,6 @@
 
 // TODO: Create a function to generate markdown for README
 
-const generateMarkdown = (projectName, githubName) => {
-  return `
-  
-  # ${title}
 
-  ## Website
-  ${description}
-  `;
-  };
 
 module.exports = generateMarkdown;
